a772d8bc1ee05d3a19f2a772dede8552c82624a2
Oduoronyina <oduoronyina@outlook.com>
2022-07-30 00:59:23 +0300

    a function that multiplies 2 matrices by using the module NumPy

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+File: 101-lazy_matrix_mul.py
+Desc: This module supplies one function called lazy_matrix_mul
+Author: Oduoronyina
+Date Created: Jul 30 2022
+"""
+from numpy import matmul as m
+
+
+def lazy_matrix_mul(m_a, m_b):
+        """
+            This function multiplies two matrices.
+                """
+                    return m(m_a, m_b)
+
