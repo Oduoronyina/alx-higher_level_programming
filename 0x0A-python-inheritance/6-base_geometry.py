cd50d9a102188b504fd8aa5a39d56d96ee2816ce
Oduoronyina <oduoronyina@gmail.com>
2022-11-01 00:14:58 +0300

    jyg

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+File: 6-base_geometry.py
+Desc: This module defines an empty class.
+Author: Oduoronyina
+Date Created: Tuesday, November 01 2022
+"""
+
+
+class BaseGeometry:
+    """
+    A simple class defination.
+    """
+    def area(self):
+        """
+        A function that raises an Exception.
+        """
+        raise Exception("area() is not implemented")
